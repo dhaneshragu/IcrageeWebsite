04d096061202b2fbd1e8852274f76b882d3e71c6
Kunalpal216 <kunalpal2115@gmail.com>
2023-09-25 02:14:10 +0530

    Base Code

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,230 @@
+import React from "react";
+import "./style.css";
+import backImage from "../../assets/images/IITGBackground.jpeg"
+const Home = () => {
+  return (
+    <div className="index">
+      <div className="div">
+        <div className="overlap-group">
+          <div className="frame">
+            <img
+              className="download"
+              alt="Download"
+              src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/download-5.png"
+            />
+            <img
+              className="screenshot"
+              alt="Screenshot"
+              src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/screenshot--90--2.png"
+            />
+          </div>
+          <div className="frame-wrapper">
+            <div className="frame-2">
+              <div className="text-wrapper">Important Dates</div>
+              <div className="text-wrapper-2">11th September 2023</div>
+              <img
+                className="line"
+                alt="Line"
+                src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/line-8.svg"
+              />
+              <p className="p">Name of the event here</p>
+              <img
+                className="img"
+                alt="Line"
+                src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/line-2.svg"
+              />
+              <div className="text-wrapper-2">11th September 2023</div>
+              <img
+                className="line"
+                alt="Line"
+                src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/line-9.svg"
+              />
+              <p className="p">Name of the event here</p>
+              <img
+                className="img"
+                alt="Line"
+                src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/line-3.svg"
+              />
+              <div className="text-wrapper-2">11th September 2023</div>
+              <img
+                className="line"
+                alt="Line"
+                src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/line-10.svg"
+              />
+              <p className="p">Name of the event here</p>
+              <img
+                className="img"
+                alt="Line"
+                src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/line-4.svg"
+              />
+              <div className="text-wrapper-2">11th September 2023</div>
+              <img
+                className="line"
+                alt="Line"
+                src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/line-11.svg"
+              />
+              <p className="p">Name of the event here</p>
+              <img
+                className="img"
+                alt="Line"
+                src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/line-5.svg"
+              />
+              <div className="text-wrapper-2">11th September 2023</div>
+              <img
+                className="line"
+                alt="Line"
+                src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/line-12.svg"
+              />
+              <p className="p">Name of the event here</p>
+              <img
+                className="img"
+                alt="Line"
+                src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/line-6.svg"
+              />
+              <div className="text-wrapper-2">11th September 2023</div>
+              <img
+                className="line"
+                alt="Line"
+                src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/line-13.svg"
+              />
+              <p className="p">Name of the event here</p>
+            </div>
+          </div>
+          <div className="frame-3">
+            <div className="about-icracee">
+              <div className="text-wrapper-3">About ICRAGEE</div>
+              <p className="short-description">
+                Short Description//Short Description//Short Description//Short Description//Short Description//Short
+                Description//Short Description//Short Description//Short Description//Short Description//Short
+                Description//Short Description//
+                <br />
+                Short Description//Short Description//Short Description//Short Description//
+              </p>
+            </div>
+            <img
+              className="element-jpg"
+              alt="Element jpg"
+              src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/95-jpg.png"
+            />
+            <img
+              className="element-jpg"
+              alt="Element jpg"
+              src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/95-jpg.png"
+            />
+            <div className="frame-4">
+              <div className="div-wrapper">
+                <div className="text-wrapper-4">7th Edition</div>
+              </div>
+              <p className="short-description-2">
+                Short Description//Short Description//Short Description//Short Description//Short Description//Short
+                Description//Short Description//Short Description//Short Description//Short Description//Short
+                Description//Short Description//
+                <br />
+                Short Description//Short Description//Short Description//Short Description//
+              </p>
+            </div>
+          </div>
+          <img
+            className="frame-5"
+            alt="Frame"
+            src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/frame-35.png"
+          />
+          <img
+            className="download-2"
+            alt="Download"
+            src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/download-2.png"
+          />
+          <div className="text-wrapper-5">8</div>
+        </div>
+        <div className="overlap">
+          <img
+            className="slide"
+            alt="Slide"
+            src={backImage}
+          />
+          <div className="nav-bar">
+            <a className="text-wrapper-6" href="#">Home</a>
+            <a className="text-wrapper-6">Organizing Committee</a>
+            <a className="text-wrapper-6">Schedule</a>
+            <a className="text-wrapper-6">Speakers</a>
+            <a className="text-wrapper-6">Workshops</a>
+            <a className="text-wrapper-6">Themes</a>
+            <a className="text-wrapper-6">Contact Us</a>
+          </div>
+        </div>
+        <div className="frame-6">
+          <div className="frame-7">
+            <div className="text-wrapper-7">Gallery</div>
+            <div className="frame-8">
+              <img
+                className="element-jpg-2"
+                alt="Element jpg"
+                src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/88-jpg@2x.png"
+              />
+              <img
+                className="element-jpg-2"
+                alt="Element jpg"
+                src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/78-jpg@2x.png"
+              />
+              <img
+                className="element-jpg-2"
+                alt="Element jpg"
+                src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/78-jpg@2x.png"
+              />
+              <img
+                className="element-jpg-2"
+                alt="Element jpg"
+                src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/95-jpg.png"
+              />
+              <img
+                className="element-jpg-2"
+                alt="Element jpg"
+                src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/59-jpg@2x.png"
+              />
+              <img
+                className="element-jpg-2"
+                alt="Element jpg"
+                src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/59-jpg@2x.png"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="frame-9">
+          <img
+            className="jpg"
+            alt="Jpg"
+            src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/logo1-jpg@2x.png"
+          />
+          <img
+            className="jpg"
+            alt="Jpg"
+            src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/logo5-jpg@2x.png"
+          />
+          <img
+            className="jpg"
+            alt="Jpg"
+            src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/logo6-jpg@2x.png"
+          />
+          <img
+            className="jpg"
+            alt="Jpg"
+            src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/logo4-jpg@2x.png"
+          />
+          <img
+            className="jpg"
+            alt="Jpg"
+            src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/logo2-jpg@2x.png"
+          />
+          <img
+            className="jpg"
+            alt="Jpg"
+            src="https://cdn.animaapp.com/projects/651073c5c032644aea5b69e9/releases/65107461ecd3f27648112d64/img/logo3-jpg@2x.png"
+          />
+        </div>
+        <div className="text-wrapper-8">Presents</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
