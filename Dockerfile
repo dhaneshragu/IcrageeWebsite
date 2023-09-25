aafd383751b8229229ec9f673a75781d41a67961
Kunalpal216 <kunalpal2115@gmail.com>
2023-09-25 12:29:56 +0530

    updated routor

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,13 @@
 FROM node:18-alpine
+WORKDIR  /app
+
+# Copy the local files to the app directory
+COPY . /app/
+
 WORKDIR  /app/frontend
+
+# Install dependencies
 RUN npm install
 
+RUN npm run build
 CMD ["npm","run","start"]
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter basename='/8icragee'>
+      <Routes>
+        <Route path='/' element={<App/>}/>
+      </Routes>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
